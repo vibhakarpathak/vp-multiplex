914f1579f73688d8d8c46f2365e7cc7fae721c14
Vibhakar Pathak <aim.vibhakar@gmail.com>
2023-06-15 18:54:40 +0530

    Updates

---
+++ precache-manifest.56e953e47f3b392bb6d0e1f0ddb16c32.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b5071db1c13dca9966f0344ea3ce543",
+    "url": "/vp-multiplex/index.html"
+  },
+  {
+    "revision": "aec42ec5c9ffc35a512e",
+    "url": "/vp-multiplex/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "edad0cc1dcb1c25bafc8",
+    "url": "/vp-multiplex/static/css/main.8a4df33d.chunk.css"
+  },
+  {
+    "revision": "aec42ec5c9ffc35a512e",
+    "url": "/vp-multiplex/static/js/2.fc4a5dad.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/vp-multiplex/static/js/2.fc4a5dad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edad0cc1dcb1c25bafc8",
+    "url": "/vp-multiplex/static/js/main.fed1083d.chunk.js"
+  },
+  {
+    "revision": "c7fe64059b62790e9889",
+    "url": "/vp-multiplex/static/js/runtime-main.71f5e413.js"
+  },
+  {
+    "revision": "f6739b828d87a2e5dcd777548dfc76fc",
+    "url": "/vp-multiplex/static/media/slider.f6739b82.jpg"
+  }
+]);
